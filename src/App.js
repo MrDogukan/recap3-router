6cbb361c255890a942eac6f87c65bedda86f0f8f
MrDogukan <aliseslikaya@gmail.com>
2021-12-18 13:12:00 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import MyNav from "./components/MyNav";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <MyNav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
